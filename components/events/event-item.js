ce68ea736370ebaa0ecd6ed3cd3a9b4734bb5067
Long Phan <longphan@Longs-Laptop.local>
2022-11-27 22:07:28 -0700

    home route styling completed

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,7 @@
-import Link from "next/link";
 import Button from "../ui/button";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+import DateIcon from "../icons/date-icon";
 import classes from "./event-item.module.css";
 
 const EventItem = props => {
@@ -22,14 +24,21 @@ const EventItem = props => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
