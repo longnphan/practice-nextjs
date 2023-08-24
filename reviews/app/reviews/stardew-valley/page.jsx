47005effca3db35e38d6a34b90a178238ae0ff83
Long Phan <longnphan@outlook.com>
2023-08-23 22:12:03 -0600

    add styling

--- reviews/app/reviews/stardew-valley/page.jsx
+++ reviews/app/reviews/stardew-valley/page.jsx
@@ -4,6 +4,13 @@ export default function StardewValleyPage() {
   return (
     <>
       <Heading>Stardew Valley</Heading>
+      <img
+        src="/images/stardew-valley.jpg"
+        alt="game image"
+        width="640"
+        height="360"
+        className="mb-2 rounded"
+      />
       <p>Review for the game.</p>
     </>
   );
